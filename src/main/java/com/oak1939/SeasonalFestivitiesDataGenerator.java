2012466e856f81e1217d58a8bcff08be3d10b1a2
Samuelh81 <samuelhornick1@gmail.com>
2023-10-22 19:25:22 -0500

    Initial commit

---
+++ src/main/java/com/oak1939/SeasonalFestivitiesDataGenerator.java
@@ -0,0 +1,11 @@
+package com.oak1939;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SeasonalFestivitiesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
